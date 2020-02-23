529383c1e4c0826d06bd419083ff8c3bfbed6359
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-23 11:58:21 -0600

    added fetch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import './App.css';
 import Header from './components/Header'
 import Persons from './components/Persons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
-      <Persons />
-    </div>
-  );
+class App extends React.Component{
+  
+  state = {
+    isLoading : true,
+    users : [],
+    error : null
+  }
+
+  fetchUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => response.json())
+      .then( data => 
+        this.setState({
+          users: data,
+          isLoading: false
+        })
+        )
+        .catch(error => this.setState({error, isLoading: false}));
+  }
+
+  componentDidMount(){
+    this.fetchUsers();
+  }
+
+  render(){
+
+    const { isLoading, error, users } = this.state;
+    console.log(users);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+        {error? <p> {error.message} </p> : null }
+        {!isLoading ? (
+          users.map(user => {
+            const { username, name, email } = user;
+            <Persons />
+          })
+        )}
+        <Persons />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Person.js
+++ src/components/Person.js
@@ -4,7 +4,6 @@ import './Person.css'
 const Person = ({ name,age }) => {
     return (
         <div className="person-card">
-            Person details are as below :
             <p>Name : {name} </p>
             <p>Age  : {age} </p>
         </div>
