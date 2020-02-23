ec7341285bc26f2d3a292379aab5ea52fca1c113
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-23 11:35:00 -0600

    checking commits

--- src/components/Person.js
+++ src/components/Person.js
@@ -4,6 +4,7 @@ import './Person.css'
 const Person = ({ name,age }) => {
     return (
         <div className="person-card">
+            Person details are as below :
             <p>Name : {name} </p>
             <p>Age  : {age} </p>
         </div>
